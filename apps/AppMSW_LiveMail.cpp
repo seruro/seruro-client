30439ac36122f3cf12e1654db4277250e74d1eaf
Teddy <teddy@prosauce.org>
2013-08-11 14:09:36 -0600

    [SER-69] Found key in HKLM to detect version of Windows Live Mail.

--- apps/AppMSW_LiveMail.cpp
+++ apps/AppMSW_LiveMail.cpp
@@ -5,6 +5,8 @@
 #include "../SeruroClient.h"
 #include "../crypto/SeruroCrypto.h"
 
+#include <wx/msw/registry.h>
+
 #include <wx/stdpaths.h>
 
 #define MAILDATA_PATH "/AppData/Local/Microsoft/Windows Live Mail/"
@@ -16,6 +18,20 @@
 /* Both fields may not exist, and should be the "thumbprint" or hash of the certificate. */
 #define XML_FIELD_AUTH "SMTP_Certificate" 
 #define XML_FIELD_ENC  "SMTP_Encryption_Certificate"
+
+/* Check if Windows Live Mail is installed. */
+/*
+ * HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\
+ * UserData\S-1-5-18\Products\D0E0789820698F14E938416F4839644A
+ *
+ * HKEY_CLASSES_ROOT\Installer\Products\D0E0789820698F14E938416F4839644A
+ *
+ * HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\D0E0789820698F14E938416F4839644A
+ */
+
+#define HKLM_KEY "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Installer\\\
+UserData\\S-1-5-18\\Products\D0E0789820698F14E938416F4839644A"
+#define HKLM_KEY_PROPERTIES "InstallProperties"
 
 #define XML_FIELD_NAME "Account_Name" /* type="SZ" */
 #define XML_FIELD_ADDRESS "SMTP_Email_Address" /* type="SZ" */
@@ -58,6 +74,24 @@ bool AppMSW_LiveMail::IsIdentityInstalled(wxString account_name)
 
 bool AppMSW_LiveMail::GetInfo()
 {
+    wxRegKey check_key(wxRegKey::HKLM, _(HKLM_KEY));
+    
+    if (! check_key.Exists() || ! check_key.HasSubKey(_(HKLM_KEY_PROPERTIES))) {
+        is_detected = false;
+        is_installed = false;
+        return false;
+    }
+    
+    wxString version;
+    wxRegKey properties_key(wxRegKey::HKLM, _(HKLM_KEY) + _(HKLM_KEY_PROPERTIES));
+    if (! properties_key.QueryValue(_("DisplayVersion"), version, false)) {
+        is_detected = false;
+        return false;
+    }
+    
+    this->info["version"] = version;
+    
+    
 	return false;
 }
 
